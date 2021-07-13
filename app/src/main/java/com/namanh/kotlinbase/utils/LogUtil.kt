24e3a455f6596c095021d8923fe1f8ddbb7952d3
Nam Anh Nguyen <namanh11611@gmail.com>
2021-07-13 09:34:44 +0700

    Refactor LogUtil

--- app/src/main/java/com/namanh/kotlinbase/utils/LogUtil.kt
+++ app/src/main/java/com/namanh/kotlinbase/utils/LogUtil.kt
@@ -6,25 +6,47 @@ import com.namanh.kotlinbase.BuildConfig
 object LogUtil {
     private const val TAG = "LogUtil"
 
+    fun v(msg: String) {
+        if (BuildConfig.DEBUG) {
+            Log.v(TAG, "[${getCallOrigin()}] $msg")
+        }
+    }
+
     fun d(msg: String) {
         if (BuildConfig.DEBUG) {
-            Log.d(TAG, msg)
+            Log.d(TAG, "[${getCallOrigin()}] $msg")
         }
     }
 
     fun i(msg: String) {
-        if (BuildConfig.DEBUG) {
-            Log.i(TAG, msg)
-        }
+        Log.i(TAG, "[${getCallOrigin()}] $msg")
     }
 
     fun w(msg: String) {
-        if (BuildConfig.DEBUG) {
-            Log.w(TAG, msg)
-        }
+        Log.w(TAG, "[${getCallOrigin()}] $msg")
     }
 
     fun e(msg: String) {
-        Log.e(TAG, msg)
+        Log.e(TAG, "[${getCallOrigin()}] $msg")
+    }
+
+    private fun getCallOrigin(): String {
+        val st = Thread.currentThread().stackTrace
+
+        // The call stack should look like:
+        //   n [a variable number of calls depending on the vm used]
+        //  +0 getCallOrigin()
+        //  +1 privateLogFunction: verbose or debug
+        //  +2 caller
+        var callerStackIndex = 0
+        val logClassName: String = LogUtil::class.java.name
+        while (callerStackIndex < st.size) {
+            if (st[callerStackIndex].className == logClassName) {
+                callerStackIndex += 2
+                break
+            }
+            callerStackIndex++
+        }
+        return st[callerStackIndex].fileName + ":" + st[callerStackIndex].lineNumber
     }
 }
